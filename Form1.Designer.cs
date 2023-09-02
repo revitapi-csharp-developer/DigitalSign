601b3c3e87e71cd2faa4f6f56efa6c3c4d8a1b2c
Ali Rıza Yılmaz <aliriza.yilmaz@eaegroup.com>
2023-09-02 22:02:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DigitalSign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(12, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(360, 36);
+            this.button4.TabIndex = 37;
+            this.button4.Text = "SIGN";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::DigitalSign.Properties.Resources.icons8_file;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(68, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 42;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(96, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 95);
+            this.textBox1.TabIndex = 39;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Sign Application";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DigitalSign
+{
+    public partial class Form1 : Form
+    {
+        public string username = string.Empty;
+        public string generalpath = string.Empty;
+        public string sourcepath_ = string.Empty;
+        public string destinationpath_ = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+         
+            textBox1.Enabled = false;
+            username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\').Last().ToString();
+
+            CheckDirectory();
+            ClearSigningPath();
+
+        }
+        void CheckDirectory()
+        {
+            generalpath = @"C:\Users\" + username + @"\AppData\Roaming\Autodesk\Revit\Signing";
+
+            if (!Directory.Exists(generalpath))
+            {
+                Directory.CreateDirectory(generalpath);
+            }
+        }
+
+        void ClearSigningPath()
+        {
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(generalpath);
+
+                if (!di.GetFiles().Count().Equals(0))
+                {
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Length.Equals(0))
+            {
+                //YAPILACAK
+                GetDllFile();
+                Process(destinationpath_);
+            }
+        }
+
+        private void GetFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Select a File";
+            openFileDialog.Filter = "Executable Files|*.dll;*.exe;*.msi|All Files (*.*)|*.*"; // Filter for specific file types
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                textBox1.Text = selectedFilePath; // Display the selected file's path in a TextBox or another control
+            }
+        }
+        void Process(string filepath)
+        {
+            try
+            {
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false; // Set this to false
+
+                // Set the working directory to the location of signtool.exe
+                cmd.StartInfo.WorkingDirectory = @"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64";
+
+                cmd.Start();
+
+                cmd.StandardInput.WriteLine(@"signtool sign /a /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256 " + filepath);
+                cmd.StandardInput.Flush();
+
+                // Remove the line that closes the input stream
+                // cmd.StandardInput.Close();
+
+                string output = cmd.StandardOutput.ReadToEnd();
+                cmd.WaitForExit();
+
+                // Display the output for debugging
+                Console.WriteLine(output);
+
+                // Check if the process exited with an error
+                //if (cmd.ExitCode != 0)
+                //{
+                //    System.Windows.Forms.MessageBox.Show("Error occurred: " + output);
+                //}
+
+                OpenSignedFile();
+            }
+            catch 
+            {
+            }
+        }
+        void GetDllFile()
+        {
+            sourcepath_ = textBox1.Text;
+            destinationpath_ = generalpath + @"\" + sourcepath_.Split('\u005c').Last();
+
+            try
+            {
+                File.Move(sourcepath_, destinationpath_);
+            }
+            catch (IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+        }
+        void OpenSignedFile()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("explorer.exe", generalpath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            GetFile();
+        }
+    }
+}
